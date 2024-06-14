e760d08ba8ccd9483ddf42b5588bc7ba5f9116a0
gulam161 <gulam.mur159@gmail.com>
2024-06-14 16:37:31 +0530

    Update

---
+++ src/components/CommonSection.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+type CommonSectionProps = {
+  heading: string;
+  subheading: string;
+  description: string;
+  link?: string;
+  linkText?: string;
+  buttons: string[];
+  images: string[];
+  listItems?: string[];
+  isReverse?: boolean;
+};
+
+const CommonSection: React.FC<CommonSectionProps> = ({
+  heading,
+  subheading,
+  description,
+  link,
+  linkText,
+  buttons,
+  images,
+  listItems,
+  isReverse,
+}) => {
+  const [active, setActive] = useState(0);
+
+  return (
+    <section
+      className={`flex ${
+        isReverse && "flex-row-reverse"
+      } border border-gray-200 text-gray-700 rounded-xl shadow-md my-32`}
+    >
+      <div
+        className={`p-9 w-1/2 ${
+          listItems
+            ? "bg-gray-100 rounded-r-xl shadow-lg"
+            : "bg-gray-100 rounded-l-xl shadow-lg"
+        }`}
+      >
+        <h4 className="text-[#189ed3] text-sm mb-1">{subheading}</h4>
+        <h1 className="text-4xl font-medium mb-4">{heading}</h1>
+        <p className="font-light tracking-wide mb-10">{description}</p>
+        {link && linkText && (
+          <Link
+            to={link}
+            className="button px-8 text-white border-[#ed5668] bg-[#ed5668]"
+          >
+            {linkText}
+          </Link>
+        )}
+        {listItems && (
+          <ul style={{ listStyleType: "disc", paddingLeft: "1.5rem" }}>
+            {listItems.map((item, idx) => (
+              <li key={idx} className="font-light tracking-wide mb-2">
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="p-9 w-1/2">
+        <div className="border w-4/5 mx-auto grid grid-cols-2 rounded-full">
+          {buttons.map((label, idx) => (
+            <button
+              key={label}
+              className={`py-2 ${
+                active === idx
+                  ? "bg-purple-900 text-white"
+                  : "bg-white text-black"
+              } rounded-${idx === 0 ? "l" : "r"}-full`}
+              onClick={() => setActive(idx)}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+        <div className="mt-8 text-center">
+          <img
+            src={images[active]}
+            alt={`${buttons[active]}_Diagram`}
+            className="mx-auto"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default CommonSection;
--- src/components/Distributed.tsx
+++ src/components/Distributed.tsx
@@ -1,73 +1,19 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import CommonSection from "./CommonSection";
 import Before_Diagram from "../assets/Before_Diagram.png";
 import After_Diagram from "../assets/After_Diagram.png";
 
-// const data = [
-//   {
-//     label: "10X YOUR VELOCITY",
-//     heading: "Supercharge coding, scaling and observing distributed systems",
-//     discription:
-//       "Orkes Conductor is the fastest way to build and modernize all your applications. Model your business logic as intuitively as you would in a whiteboard, code the components in the language and framework of your choice, run them at scale with no additional setups and observe across your distributed landscape - with enterprise grade security and manageability baked-in.",
-//     link: "",
-//     text: "Get Started",
-//     button1: "Before",
-//     button2: "After",
-//     image1: "Before_Diagram",
-//     image2: "After_Diagram",
-//   },
-// ];
-
 const Distributed: React.FC = () => {
-  const [active, setActive] = useState(true);
-
   return (
-    <section className="grid grid-cols-2 border border-gray-200 text-gray-700 rounded-xl shadow-md my-32">
-      <div className="p-8 bg-gray-100 rounded-l-xl shadow-lg">
-        <h4 className="text-[#189ed3] text-base mb-1">10X YOUR VELOCITY</h4>
-        <h1 className="text-4xl font-medium mb-4">
-          Supercharge coding, scaling and observing distributed systems
-        </h1>
-        <p className="font-light tracking-wide mb-10">
-          Orkes Conductor is the fastest way to build and modernize all your
-          applications. Model your business logic as intuitively as you would in
-          a whiteboard, code the components in the language and framework of
-          your choice, run them at scale with no additional setups and observe
-          across your distributed landscape - with enterprise grade security and
-          manageability baked-in.
-        </p>
-        <Link
-          to=""
-          className="button px-8 text-white border-[#ed5668] bg-[#ed5668]"
-        >
-          Get Started
-        </Link>
-      </div>
-      <div className="p-8">
-        <div className="border w-4/5 mx-auto grid grid-cols-2 rounded-full">
-          {["Before", "After"].map((label, idx) => (
-            <button
-              key={label}
-              className={`py-2 ${
-                active === (idx === 0)
-                  ? "bg-purple-900 text-white"
-                  : "bg-white text-black"
-              } rounded-${idx === 0 ? "l" : "r"}-full`}
-              onClick={() => setActive(idx === 0)}
-            >
-              {label}
-            </button>
-          ))}
-        </div>
-        <div className="mt-8 text-center">
-          <img
-            src={active ? Before_Diagram : After_Diagram}
-            alt={`${active ? "Before" : "After"}_Diagram`}
-            className="mx-auto"
-          />
-        </div>
-      </div>
-    </section>
+    <CommonSection
+      heading="Supercharge coding, scaling and observing distributed systems"
+      subheading="10X YOUR VELOCITY"
+      description="Orkes Conductor is the fastest way to build and modernize all your applications. Model your business logic as intuitively as you would in a whiteboard, code the components in the language and framework of your choice, run them at scale with no additional setups and observe across your distributed landscape - with enterprise grade security and manageability baked-in."
+      link="#"
+      linkText="Get Started"
+      buttons={["Before", "After"]}
+      images={[Before_Diagram, After_Diagram]}
+    />
   );
 };
 
