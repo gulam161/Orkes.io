4a825693c158f3eaeb404598041f38003c43487a
gulam161 <gulam.mur159@gmail.com>
2024-07-26 14:17:16 +0530

    Update

--- src/components/Plateform/platform/Plateform.tsx
+++ src/components/Plateform/platform/Plateform.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../Navigation/Header";
 import HeroPage_2 from "./HeroPage_2";
 import Standard_Section from "../../HomePage/Standard_Section";
+import Workflow_engine from "./Workflow_engine";
 
 type PlateformProps = {};
 
@@ -12,6 +13,7 @@ const Plateform: React.FC<PlateformProps> = () => {
       <div className="w-4/5 mx-auto flex_items_between">
         <HeroPage_2 />
         <Standard_Section />
+        <Workflow_engine />
       </div>
     </>
   );
---
+++ src/components/Plateform/platform/Workflow_engine.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Platform_Page_Workflow_left from "../../../assets/Platform-Page-_Workflow-engine-left-p-800.png";
+import Platform_Page_Workflow_right from "../../../assets/Platform-Page-_Workflow-engine-right-p-800.png";
+
+type Workflow_engineProps = {};
+
+const Workflow_engine: React.FC<Workflow_engineProps> = () => {
+  return (
+    <section className="mx-auto my-12 flex w-[100%] h-[350px] rounded-xl grid_shaddow bg-gray-100 relative overflow-hidden">
+      <img
+        src={Platform_Page_Workflow_left}
+        alt={Platform_Page_Workflow_left}
+        className="absolute w-[320px] top-auto bottom-auto left-[-5%] right-auto"
+      />
+
+      <div className="text-center flex flex-col justify-center gap-4 relative z-10 px-40 tracking-wide">
+        <h1 className="text-3xl font-medium">What is a workflow engine?</h1>
+        <p className="font-light text-gray-700">
+          A workflow engine takes business logic and processes and simplifies
+          them into individual blocks or “tasks.” From there each of these tasks
+          can become autonomous microservices/ serverless/ APIs and can be
+          individually scaled without affecting the entire application.
+        </p>
+        <p className="font-light text-gray-700">
+          By utilizing a workflow engine, developers can focus on building
+          mission critical applications rather than the mechanics of service
+          communication, flow durability, and failure handling.
+        </p>
+      </div>
+      <img
+        width={100}
+        src={Platform_Page_Workflow_right}
+        alt={Platform_Page_Workflow_right}
+        className="absolute w-[320px] top-auto bottom-auto right-[-5%] left-auto"
+      />
+    </section>
+  );
+};
+export default Workflow_engine;
